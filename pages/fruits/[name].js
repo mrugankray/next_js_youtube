7e2382756ce385a170c3e01784434916eb993a7c
mrugankray <mrugankray2@gmail.com>
2021-05-03 15:41:05 +0530

    Using getStaticProps to generate static pages

--- pages/fruits/[name].js
+++ pages/fruits/[name].js
@@ -1,6 +1,7 @@
 import React from 'react'
+import { useRouter } from "next/router"
 
-export const getServerSideProps = async (context) => {
+export const getStaticProps = async (context) => {
     console.log(context)
     const response = await fetch(`http://localhost:3000/api/fruits/${context.params.name}`).then(res => res.json())
 
@@ -9,8 +10,21 @@ export const getServerSideProps = async (context) => {
     }
 }
 
+export const getStaticPaths = () => {
+    return {
+        paths: [
+            {params: {name: "apple"}}
+        ],
+        fallback: "blocking"
+    }
+}
+
 function  name(props) {
     console.log(props)
+    const router = useRouter()
+    if(router.isFallback) {
+        return (<h1>Loading</h1>)
+    }
     return (
         <> 
             <div>
--- pages/fruits/random.js
+++ pages/fruits/random.js
@@ -1,10 +1,15 @@
 import React from 'react'
 
-export const getStaticProps = async (context) => {
+export const getStaticProps = async(context) => {
     return {
-        props: {random: Math.random()}
+        props: {random: Math.random()},
+        revalidate: 10
     }
 }
+
+// sec:       0  1  10 11 12
+//  visitors: 1  2  3  4  5
+// verison:   1  1  1  1  2
 
 function random(props) {
     return (
